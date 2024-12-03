20558b6cb856a03ca6f5664162af1f5c08e0f4d1
Oussama BOUMOUR <oussamaboumour7@gmail.com>
2024-12-03 18:50:55 +0100

    :heavy_minus_sign: TDD > YatzyTest > cycle 3 > test_1s > failing test > reduce args number to one

--- src/test/java/org/codingdojo/yatzy/YatzyTest.java
+++ src/test/java/org/codingdojo/yatzy/YatzyTest.java
@@ -21,10 +21,10 @@ public class YatzyTest {
     }
 
     @Test public void test_1s() {
-        assertTrue(Yatzy.ones(1,2,3,4,5) == 1);
-        assertEquals(2, Yatzy.ones(1,2,1,4,5));
-        assertEquals(0, Yatzy.ones(6,2,2,4,5));
-        assertEquals(4, Yatzy.ones(1,2,1,1,1));
+        assertTrue(Yatzy.ones(new int[]{1,2,3,4,5}) == 1);
+        assertEquals(2, Yatzy.ones(new int[]{1,2,1,4,5}));
+        assertEquals(0, Yatzy.ones(new int[]{6,2,2,4,5}));
+        assertEquals(4, Yatzy.ones(new int[]{1,2,1,1,1}));
     }
 
     @Test
