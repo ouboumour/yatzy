7d2100343478d7ef8cad4505521bb37423c1f00e
Oussama BOUMOUR <oussamaboumour7@gmail.com>
2024-12-03 18:57:06 +0100

    :recycle: TDD > YatzyTest > cycle 3 > test_1s > refacto

--- src/test/java/org/codingdojo/yatzy/YatzyTest.java
+++ src/test/java/org/codingdojo/yatzy/YatzyTest.java
@@ -14,14 +14,16 @@ public class YatzyTest {
         assertEquals(16, Yatzy.chance(new int[]{3,3,4,5,1}));
     }
 
-    @Test public void givenDice_whenYatzy_thenReturnFiftyIfAllMatchElseZero() {
+    @Test
+    public void givenDice_whenYatzy_thenReturnFiftyIfAllMatchElseZero() {
         assertEquals(50, Yatzy.yatzy(new int[] {4,4,4,4,4}));
         assertEquals(50, Yatzy.yatzy(new int[]{6,6,6,6,6}));
         assertEquals(0, Yatzy.yatzy(new int[]{6,6,6,6,3}));
     }
 
-    @Test public void test_1s() {
-        assertTrue(Yatzy.ones(new int[]{1,2,3,4,5}) == 1);
+    @Test
+    public void givenDice_whenOnes_thenReturnOneOccurrences() {
+        assertEquals(1, Yatzy.ones(new int[]{1,2,3,4,5}));
         assertEquals(2, Yatzy.ones(new int[]{1,2,1,4,5}));
         assertEquals(0, Yatzy.ones(new int[]{6,2,2,4,5}));
         assertEquals(4, Yatzy.ones(new int[]{1,2,1,1,1}));
