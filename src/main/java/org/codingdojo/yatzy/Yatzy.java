04805f43043e9b99af8322dfce120cdd7b436f3c
Oussama BOUMOUR <oussamaboumour7@gmail.com>
2024-12-03 19:02:06 +0100

    :heavy_plus_sign: TDD > YatzyTest > cycle 4 > test_2s > passing test > reduce args number to one

--- src/main/java/org/codingdojo/yatzy/Yatzy.java
+++ src/main/java/org/codingdojo/yatzy/Yatzy.java
@@ -16,14 +16,8 @@ public class Yatzy {
         return (int) Arrays.stream(dice).filter(d -> d == 1).count();
     }
 
-    public static int twos(int d1, int d2, int d3, int d4, int d5) {
-        int sum = 0;
-        if (d1 == 2) sum += 2;
-        if (d2 == 2) sum += 2;
-        if (d3 == 2) sum += 2;
-        if (d4 == 2) sum += 2;
-        if (d5 == 2) sum += 2;
-        return sum;
+    public static int twos(int[] dice) {
+        return Arrays.stream(dice).filter(d -> d == 2).sum();
     }
 
     public static int threes(int d1, int d2, int d3, int d4, int d5) {
