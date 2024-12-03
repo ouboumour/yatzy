5916db038f000acdd3f38dce3733243f9627943b
Oussama BOUMOUR <oussamaboumour7@gmail.com>
2024-12-03 18:13:27 +0100

    :heavy_plus_sign: TDD > YatzyTest > chance_scores_sum_of_all_dice > cycle 1 > passing test > reduce args number to one

--- src/main/java/org/codingdojo/yatzy/Yatzy.java
+++ src/main/java/org/codingdojo/yatzy/Yatzy.java
@@ -1,16 +1,12 @@
 package org.codingdojo.yatzy;
 
+import java.util.Arrays;
+
 public class Yatzy {
 
-    public static int chance(int d1, int d2, int d3, int d4, int d5)
+    public static int chance(int[] dice)
     {
-        int total = 0;
-        total += d1;
-        total += d2;
-        total += d3;
-        total += d4;
-        total += d5;
-        return total;
+        return Arrays.stream(dice).sum();
     }
 
     public static int yatzy(int... dice)
