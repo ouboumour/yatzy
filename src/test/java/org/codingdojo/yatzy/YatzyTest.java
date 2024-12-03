371e7a699d64a18a5c6504702904c2773658e9c9
Oussama BOUMOUR <oussamaboumour7@gmail.com>
2024-12-03 18:38:58 +0100

    :heavy_minus_sign: TDD > YatzyTest > cycle 2 > yatzy_scores_50 > failing test > pass arg as arg instead of a varargs

--- src/test/java/org/codingdojo/yatzy/YatzyTest.java
+++ src/test/java/org/codingdojo/yatzy/YatzyTest.java
@@ -16,10 +16,10 @@ public class YatzyTest {
 
     @Test public void yatzy_scores_50() {
         int expected = 50;
-        int actual = Yatzy.yatzy(4,4,4,4,4);
+        int actual = Yatzy.yatzy(new int[] {4,4,4,4,4});
         assertEquals(expected, actual);
-        assertEquals(50, Yatzy.yatzy(6,6,6,6,6));
-        assertEquals(0, Yatzy.yatzy(6,6,6,6,3));
+        assertEquals(50, Yatzy.yatzy(new int[]{6,6,6,6,6}));
+        assertEquals(0, Yatzy.yatzy(new int[]{6,6,6,6,3}));
     }
 
     @Test public void test_1s() {
